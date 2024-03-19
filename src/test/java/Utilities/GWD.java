08ca5acbbcbdd1d65897150220fe0868e98dc919
TechnoStudy <ismettechno@gmail.com>
2024-03-19 17:51:21 +0000

    GWD 2

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -1,6 +1,9 @@
 package Utilities;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class GWD {
 
@@ -8,6 +11,13 @@ public class GWD {
 
 
     public static WebDriver getDriver(){
+
+        if (driver == null) { //1 kez oluştur
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        }
+
         return driver;
     }
 
