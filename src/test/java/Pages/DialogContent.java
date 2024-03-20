33d1363fcda5c222d19c322fadc5e002653614c0
TechnoStudy <ismettechno@gmail.com>
2024-03-20 18:13:12 +0000

    _02_CountrySteps locatorlar

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -23,4 +23,19 @@ public class DialogContent extends ParentPage{
     @FindBy(css="span[class='mat-mdc-tooltip-trigger logo-text']")
     public WebElement headText;
 
+    @FindBy(xpath = "//ms-add-button[contains(@tooltip,'ADD')]//button")
+    public WebElement addButton;
+
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='name']//input")
+    public WebElement nameInput;
+
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='code' ]//input")
+    public WebElement codeInput;
+
+    @FindBy(xpath = "//ms-save-button/button")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//div[contains(text(),'successfully')]")
+    public WebElement successMessage;
+
 }
--- src/test/java/StepDefinitions/_02_CountrySteps.java
+++ src/test/java/StepDefinitions/_02_CountrySteps.java
@@ -1,17 +1,30 @@
 package StepDefinitions;
 
+import Pages.DialogContent;
+import Pages.LeftNav;
 import io.cucumber.java.en.*;
 
 public class _02_CountrySteps {
+    LeftNav ln=new LeftNav();
+    DialogContent dc=new DialogContent();
+
     @And("Navigate to country")
     public void navigateToCountry() {
+      ln.myClick(ln.setup);
+      ln.myClick(ln.parameters);
+      ln.myClick(ln.countries);
     }
 
     @When("Create a country")
     public void createACountry() {
+       dc.myClick(dc.addButton);
+       dc.mySendKeys(dc.nameInput,"ulkeadi");
+       dc.mySendKeys(dc.codeInput,"ulkekod");
+       dc.myClick(dc.saveButton);
     }
 
     @Then("Success message should be displayed")
     public void successMessageShouldBeDisplayed() {
+       // success yazısını doğrula
     }
 }
