46b07688c25d9544bc8068e64ed9c8259cc72f44
TechnoStudy <ismettechno@gmail.com>
2024-03-20 17:18:35 +0000

    _02_CountrySteps fonksiyonlar ototmatik oluşturuldu

---
+++ src/test/java/StepDefinitions/_02_CountrySteps.java
@@ -0,0 +1,17 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class _02_CountrySteps {
+    @And("Navigate to country")
+    public void navigateToCountry() {
+    }
+
+    @When("Create a country")
+    public void createACountry() {
+    }
+
+    @Then("Success message should be displayed")
+    public void successMessageShouldBeDisplayed() {
+    }
+}
