8cd722b25219ec8f3f6b572f89b484db5f62540c
TechnoStudy <ismettechno@gmail.com>
2024-03-20 18:55:23 +0000

    Citizenships senaryo tamamlandı

--- src/test/java/Pages/LeftNav.java
+++ src/test/java/Pages/LeftNav.java
@@ -20,4 +20,7 @@ public class LeftNav extends ParentPage{
     @FindBy(xpath = "(//span[text()='Countries'])[1]")
     public WebElement countries;
 
+    @FindBy(xpath = "(//span[text()='Citizenships'])[1]")
+    public WebElement citizenship;
+
 }
