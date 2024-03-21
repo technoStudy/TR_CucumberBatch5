a7eb0bcb2581966647722fe274152a78d4c81563
TechnoStudy <ismettechno@gmail.com>
2024-03-21 17:13:43 +0000

    Citizenship Functionality

--- src/test/java/StepDefinitions/_03_CitizenShip.java
+++ src/test/java/StepDefinitions/_03_CitizenShip.java
@@ -3,6 +3,7 @@ package StepDefinitions;
 import Pages.DialogContent;
 import Pages.LeftNav;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.commons.lang3.RandomStringUtils;
 
@@ -28,4 +29,16 @@ public class _03_CitizenShip {
         dc.myClick(dc.saveButton);
     }
 
+    @When("Create a CitizenShip name as {string} shortkod as {string}")
+    public void createACitizenShipNameAsShortkodAs(String name, String shortKod) {
+        dc.myClick(dc.addButton);
+        dc.mySendKeys(dc.nameInput, name);
+        dc.mySendKeys(dc.shortName, shortKod);
+        dc.myClick(dc.saveButton);
+    }
+
+    @Then("Already exist message should be displayed")
+    public void alreadyExistMessageShouldBeDisplayed() {
+        dc.verifyContainsText(dc.alreadyExist,"already");
+    }
 }
