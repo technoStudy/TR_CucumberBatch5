2d06ce8bb80ae67f9798d077d3bda481e8b4319d
TechnoStudy <ismettechno@gmail.com>
2024-03-28 16:47:17 +0000

    pom xml e slf4j versiyon değiştirildi

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 import java.util.Locale;
 
+
 public class GWD {
 
     private static WebDriver driver;
 
 
     public static WebDriver getDriver(){
+
         Locale.setDefault(new Locale("EN"));
         System.setProperty("user.language", "EN");
 
