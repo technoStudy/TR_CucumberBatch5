1366a03200b65a01074b2f04b10b95bf7cf5a4ea
TechnoStudy <ismettechno@gmail.com>
2024-04-02 19:09:00 +0100

    __07_Soru1

---
+++ src/test/java/ApachePOI/_07_Soru1.java
@@ -0,0 +1,30 @@
+package ApachePOI;
+
+/**
+ *  Ekrandaki gibi bir Excel Çarpım tablosunu
+ *  varolan boş bir excel dosyasına
+ *  1 den 10 kadar çarpımlar için yapınız.
+ *  (ipucu önce sadece 1 leri yapınız sonra
+ *  diğerlerini)
+ */
+//        1 x 1 = 1
+//        1 x 2 = 2
+//        1 x 3 = 3
+//        1 x 4 = 4
+//        1 x 5 = 5
+//        1 x 6 = 6
+//        1 x 7 = 7
+//        1 x 8 = 8
+//        1 x 9 = 9
+//        1 x 10 = 10
+
+
+public class _07_Soru1 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
