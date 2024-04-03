b1fc8236a1615501ca2f7de6f1584c585f6b0905
TechnoStudy <ismettechno@gmail.com>
2024-04-03 19:24:31 +0100

    _11_Soru 1

--- src/test/java/ApachePOI/_11_Soru.java
+++ src/test/java/ApachePOI/_11_Soru.java
@@ -7,9 +7,29 @@ package ApachePOI;
    yazısını ekleyen metodu yazınız.
    Yani dosya yoksa oluştur ilk satıra yaz, sonra dosya oluşacağı için
    dosya varsa en son satırın altına yaz.
+
+   File dosya= new File(path); //dosya kontrolü
+
+if (!file.exists()) //dosya yok ise
+{
+  oluşturup
+  merhaba düznya yazsın
+}
+else
+{
+   var olan dosyaya eklesin
+  }
+
  */
 
 public class _11_Soru {
 
+    public static void main(String[] args) {
+        String path="";
+        String testSonuc="Test Passed";
+
+        writeToExcel(path, testSonuc);
+
+    }
 
 }
