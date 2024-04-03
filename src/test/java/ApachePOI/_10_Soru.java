e90cfa2d918e8431e117eddd463d4c98221d3f7d
TechnoStudy <ismettechno@gmail.com>
2024-04-03 18:18:56 +0100

    _10_Soru

---
+++ src/test/java/ApachePOI/_10_Soru.java
@@ -0,0 +1,17 @@
+package ApachePOI;
+
+/*
+   Main den bir metod çağırmak suretiyle, path i ve sheetName i verilen excelden
+   istenilen sütun kadar veriyi okuyup bir List e atınız.
+   Bu soruda kaynak Excel için : ApacheExcel2.xlsx  in 2.sheet ini kullanabilirsiniz.
+ */
+
+public class _10_Soru {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
