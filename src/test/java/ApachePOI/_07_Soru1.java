59ccc44a3c52a16d4c3bba77af3b56cceffba2b9
TechnoStudy <ismettechno@gmail.com>
2024-04-03 17:22:16 +0100

    _07_Soru1 çözüm

--- src/test/java/ApachePOI/_07_Soru1.java
+++ src/test/java/ApachePOI/_07_Soru1.java
@@ -1,10 +1,9 @@
 package ApachePOI;
 
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -34,17 +33,31 @@ public class _07_Soru1 {
         Workbook workbook= WorkbookFactory.create(inputStream);
         Sheet sheet=workbook.getSheetAt(0);
 
-
-        for (int i = 1; i <= 10; i++) {
+        int yeniSatirIndex=0;
+        for (int i = 1; i <= 10; i++) { //
 
             for (int j = 1; j <= 10 ; j++) {
-                System.out.println(i+"x"+j+"="+(i*j));
+                Row yeniSatir= sheet.createRow(yeniSatirIndex++);
+                //System.out.println(i+"x"+j+"="+(i*j));
 
+                Cell hucre1= yeniSatir.createCell(0); hucre1.setCellValue(i);
+                Cell hucre2= yeniSatir.createCell(1); hucre2.setCellValue("x");
+                Cell hucre3= yeniSatir.createCell(2); hucre3.setCellValue(j);
+                Cell hucre4= yeniSatir.createCell(3); hucre4.setCellValue("=");
+                Cell hucre5= yeniSatir.createCell(4); hucre5.setCellValue(i*j);
             }
 
+            Row bosSatir= sheet.createRow(yeniSatirIndex++);
         }
 
+        inputStream.close();  //okuma modunu kapattım
 
+        FileOutputStream outputStream=new FileOutputStream(path);
+        workbook.write(outputStream);
+        workbook.close();
+        outputStream.close();
+
+        System.out.println("İşlem tamamlandı");
     }
 
 
