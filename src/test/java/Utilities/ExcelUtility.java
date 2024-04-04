1dc2a3647ea3b4f6eb1c11a347ef7fa2f87259b4
TechnoStudy <ismettechno@gmail.com>
2024-04-04 17:06:15 +0100

    ExcelUtility hazırlandı

--- src/test/java/Utilities/ExcelUtility.java
+++ src/test/java/Utilities/ExcelUtility.java
@@ -1,10 +1,12 @@
 package Utilities;
 
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 public class ExcelUtility {
@@ -33,6 +35,66 @@ public class ExcelUtility {
         }
 
         return tablo;
+    }
+
+    public static void writeToExcel(String path, String testSonuc){
+        File dosya=new File(path);
+
+        if (!dosya.exists()) // dosya yoksa
+        {
+            XSSFWorkbook workbook=new XSSFWorkbook();
+            XSSFSheet sheet=workbook.createSheet("Senaryolar");
+
+            Row yeniSatir=sheet.createRow(0);
+            Cell yeniHucre=yeniSatir.createCell(0);
+            yeniHucre.setCellValue(testSonuc);
+
+            try {
+                FileOutputStream outputStream = new FileOutputStream(path);
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+            }
+            catch (Exception ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+            System.out.println("Dosya yok ise bölümü çalıştı");
+        }
+        else // dosya var ise
+        {
+            FileInputStream inputStream=null;
+            Workbook workbook=null;
+            Sheet sheet =null;
+
+            try {
+                inputStream = new FileInputStream(path);
+                workbook = WorkbookFactory.create(inputStream);
+                sheet = workbook.getSheetAt(0);
+            }
+            catch (Exception ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+
+            int satirMiktar=sheet.getPhysicalNumberOfRows();
+            Row yeniSatir=sheet.createRow(satirMiktar);
+            Cell yeniHucre=yeniSatir.createCell(0);
+            yeniHucre.setCellValue(testSonuc);
+
+            try {
+                inputStream.close();
+                FileOutputStream outputStream = new FileOutputStream(path);
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+            }
+            catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+
+            System.out.println("Dosya VAR ise bölümü çalıştı");
+        }
     }
 
 }
