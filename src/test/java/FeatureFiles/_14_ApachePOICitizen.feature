a33950602f9280824425b9bb79e8ee5a9c511ff6
TechnoStudy <ismettechno@gmail.com>
2024-04-04 17:20:35 +0100

    _09_ApachePOISteps

---
+++ src/test/java/FeatureFiles/_14_ApachePOICitizen.feature
@@ -0,0 +1,10 @@
+Feature: ApachePOI Citizen functionality
+
+  Background:
+    Given Navigate to Campus
+    When Enter username and password and click login button
+    Then User should login successfully
+
+  Scenario: Create and Delete CitizenShip from excel
+    When User create citizenship with ApchePOI
+    Then User delete citizenship with ApchePOI
---
+++ src/test/java/StepDefinitions/_09_ApachePOISteps.java
@@ -0,0 +1,40 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import Utilities.ExcelUtility;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.ArrayList;
+
+public class _09_ApachePOISteps {
+    DialogContent dc=new DialogContent();
+    @When("User create citizenship with ApchePOI")
+    public void userCreateCitizenshipWithApchePOI() {
+
+        ArrayList< ArrayList<String> > tablo= ExcelUtility.getData(
+                "src/test/java/ApachePOI/resource/ApacheExcel2.xlsx",
+                "testCitizen", 2);
+
+        for (ArrayList<String> satir : tablo){
+            dc.myClick(dc.addButton);
+            dc.mySendKeys(dc.nameInput, satir.get(0));
+            dc.mySendKeys(dc.shortName, satir.get(1));
+            dc.myClick(dc.saveButton);
+            dc.verifyContainsText(dc.successMessage, "success");
+        }
+
+    }
+
+    @Then("User delete citizenship with ApchePOI")
+    public void userDeleteCitizenshipWithApchePOI() {
+        ArrayList< ArrayList<String> > tablo= ExcelUtility.getData(
+                "src/test/java/ApachePOI/resource/ApacheExcel2.xlsx",
+                "testCitizen", 2);
+
+        for (ArrayList<String> satir : tablo){
+            dc.deleteItem(satir.get(0));
+            dc.verifyContainsText(dc.successMessage, "success");
+        }
+    }
+}
