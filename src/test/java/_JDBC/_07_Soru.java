99d69f416ecf781625da6ca04991656d6856b599
TechnoStudy <ismettechno@gmail.com>
2024-04-22 19:52:45 +0100

    _07_Soru çözüm 2

--- src/test/java/_JDBC/_07_Soru.java
+++ src/test/java/_JDBC/_07_Soru.java
@@ -35,4 +35,34 @@ public class _07_Soru extends JDBCParent{
         // TODO : daha düzenli şekilde printf le isteyen yapsın
     }
 
+
+    @Test
+    public void Test2() throws SQLException {  //2.Çözüm
+        DBConnectionOpen();
+        ResultSet rs=sorguEkrani.executeQuery("select * from language");
+
+        ResultSetMetaData rsmd=rs.getMetaData();
+
+        for (int i = 1; i <= rsmd.getColumnCount(); i++)
+            System.out.print(rsmd.getColumnName(i)+"\t");
+
+        System.out.println();
+
+        rs.last();
+        int sonSatirNo=rs.getRow();
+
+        for (int i = 1; i <= sonSatirNo; i++) {
+
+            rs.absolute(i);
+            //bulunduğun satırdaki bütün kolonları yazdır
+            for (int j = 1; j <= rsmd.getColumnCount(); j++)
+                System.out.print(rs.getString(j)+"\t");
+
+            System.out.println();
+        }
+
+
+        DBConnectionClose();
+        // TODO : daha düzenli şekilde printf le isteyen yapsın
+    }
 }
