f6b745c65e3a978b0d7e6bd1402564cf503dd1b9
TechnoStudy <ismettechno@gmail.com>
2024-04-22 19:56:57 +0100

    _08_GetAllRowColumn

---
+++ src/test/java/_JDBC/_08_GetAllRowColumn.java
@@ -0,0 +1,11 @@
+package _JDBC;
+
+public class _08_GetAllRowColumn {
+
+    // bir metoda sorguyu("select * from language") gönderiniz,
+    // metod size sorgunun datasını bir ArrayList olarak döndürsün
+
+
+
+
+}
