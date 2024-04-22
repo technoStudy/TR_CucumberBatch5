4a2b24161d216bc16d8d6ec20e7f62585747a246
TechnoStudy <ismettechno@gmail.com>
2024-04-22 19:50:02 +0100

    _07_Soru çözüm

--- src/test/java/_JDBC/_07_Soru.java
+++ src/test/java/_JDBC/_07_Soru.java
@@ -1,8 +1,38 @@
 package _JDBC;
 
-public class _07_Soru {
+import org.testng.annotations.Test;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+public class _07_Soru extends JDBCParent{
     // language tablosundaki tüm satırları ve tüm sütunları yazdırınız,
     // aynı mysql sonuç ekranında olduğu gibi
 
+    @Test
+    public void Test1() throws SQLException {
+        DBConnectionOpen();
+        ResultSet rs=sorguEkrani.executeQuery("select * from language");
+
+        ResultSetMetaData rsmd=rs.getMetaData();
+
+        for (int i = 1; i <= rsmd.getColumnCount(); i++)
+            System.out.print(rsmd.getColumnName(i)+"\t");
+
+        System.out.println();
+        while (rs.next()){ // sonraki satır var ise oraya geç
+
+            //bulunduğun satırdaki bütün kolonları yazdır
+            for (int i = 1; i <= rsmd.getColumnCount(); i++)
+              System.out.print(rs.getString(i)+"\t");
+
+            System.out.println();
+        }
+
+
+        DBConnectionClose();
+        // TODO : daha düzenli şekilde printf le isteyen yapsın
+    }
 
 }
