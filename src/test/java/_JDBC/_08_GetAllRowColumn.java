b02f5b1bf256d0a3241bb532bf577f2112d0afb5
TechnoStudy <ismettechno@gmail.com>
2024-04-23 17:52:41 +0100

    _08_GetAllRowColumn

--- src/test/java/_JDBC/_08_GetAllRowColumn.java
+++ src/test/java/_JDBC/_08_GetAllRowColumn.java
@@ -1,10 +1,58 @@
 package _JDBC;
 
-public class _08_GetAllRowColumn {
+import org.testng.annotations.Test;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _08_GetAllRowColumn extends JDBCParent{
 
     // bir metoda sorguyu("select * from language") gönderiniz,
     // metod size sorgunun datasını bir ArrayList olarak döndürsün
 
+
+    @Test
+    public void Test1()
+    {
+        String sorgu="select * from language";
+        List<List<String>>  donenTablo=getListData(sorgu); //giden var, dönen var
+        System.out.println(donenTablo);
+    }
+
+    public List<List<String>> getListData(String sorgu)
+    {
+        List<List<String>> tablo=new ArrayList<>();
+
+        try {
+            DBConnectionOpen();
+            ResultSet rs = sorguEkrani.executeQuery(sorgu);
+            ResultSetMetaData rsmd = rs.getMetaData(); // kolon sayısı
+
+            while (rs.next()) {
+
+                ArrayList<String> satir = new ArrayList<>();
+                // bu satırdaki elemanları satır listine ekle
+                // rs.getString(1); // 1
+                // rs.getString(2); // English
+                for (int i = 1; i <= rsmd.getColumnCount(); i++)
+                    satir.add(rs.getString(i));
+
+                // satıra ekleme bitince tablo ya ekle
+                tablo.add(satir);
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+        finally {
+            DBConnectionClose();
+        }
+
+        return tablo;
+    }
 
 
 
